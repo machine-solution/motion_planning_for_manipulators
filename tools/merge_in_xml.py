73fb441937f462a09115568d1a1cc03fd73d44e5
amogus <69348298+ThirdAmethystEye@users.noreply.github.com>
2023-07-06 15:59:46 +0300

    Add files via upload

--- tools/merge_in_xml.py
+++ tools/merge_in_xml.py
@@ -1,6 +1,16 @@
-dof = 2
+joints = 2
 obstacles = 6
-with open(f"tools/{dof}-dof/manipulator.xml", "w+") as f:
+
+with open("tools/collision_section.txt", "w+") as f:
+    for i in range(joints):
+        for j in range(obstacles):
+            f.write(' ' * 8 + f'<pair geom1="geom edge {i}" geom2="geom obstacle {j}"/>\n')
+    for i in range(joints):
+        for j in range(joints):
+            if j - i > 1:
+                f.write(' ' * 8 + f'<pair geom1="geom edge {i}" geom2="geom edge {j}"/>\n')
+
+with open(f"tools/{joints}-dof/manipulator.xml", "w+") as f:
     f.write(open('tools/header.txt', 'r').read())
     f.write('\n')
     f.write(open('tools/edges.txt', 'r').read())
@@ -8,6 +18,14 @@ with open(f"tools/{dof}-dof/manipulator.xml", "w+") as f:
     f.write('\n')
     f.write('    </worldbody>\n\n')
     f.write('    <contact>\n')
-    f.write(open('tools/collision_section.txt', 'r').read())
+    
+    for i in range(joints):
+        for j in range(obstacles):
+            f.write(' ' * 8 + f'<pair geom1="geom edge {i}" geom2="geom obstacle {j}"/>\n')
+    for i in range(joints):
+        for j in range(joints):
+            if j - i > 1:
+                f.write(' ' * 8 + f'<pair geom1="geom edge {i}" geom2="geom edge {j}"/>\n')
+    
     f.write('    </contact>\n')
     f.write(open('tools/footer.txt', 'r').read())
