6dff8b8cd1ecf8e3bd4b7a9f219f5361ffe6af15
amogus <69348298+ThirdAmethystEye@users.noreply.github.com>
2023-07-07 11:21:53 +0300

    Update merge_in_xml.py

--- tools/merge_in_xml.py
+++ tools/merge_in_xml.py
@@ -1,17 +1,8 @@
 joints = 2
 obstacles = 6
 
-with open("tools/collision_section.txt", "w+") as f:
-    for i in range(joints):
-        for j in range(obstacles):
-            f.write(' ' * 8 + f'<pair geom1="geom edge {i}" geom2="geom obstacle {j}"/>\n')
-    for i in range(joints):
-        for j in range(joints):
-            if j - i > 1:
-                f.write(' ' * 8 + f'<pair geom1="geom edge {i}" geom2="geom edge {j}"/>\n')
-
 with open(f"tools/{joints}-dof/manipulator.xml", "w+") as f:
-    f.write(open('tools/header.txt', 'r').read())
+    f.write(open('tools/const/header.txt', 'r').read())
     f.write('\n')
     f.write(open('tools/edges.txt', 'r').read())
     f.write(open('tools/obstacles.txt', 'r').read())
@@ -28,4 +19,4 @@ with open(f"tools/{joints}-dof/manipulator.xml", "w+") as f:
                 f.write(' ' * 8 + f'<pair geom1="geom edge {i}" geom2="geom edge {j}"/>\n')
     
     f.write('    </contact>\n')
-    f.write(open('tools/footer.txt', 'r').read())
+    f.write(open('tools/const/footer.txt', 'r').read())
