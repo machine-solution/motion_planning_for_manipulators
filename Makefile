93f3402183b1117d1b3fd062609c236a19781b1d
ThirdAmetistEye <nn0uneim@yandex.ru>
2023-07-14 21:10:13 +0300

    creation of integration test

--- Makefile
+++ Makefile
@@ -11,19 +11,26 @@ INCLUDES = $(INC)/utils.h $(INC)/joint_state.h $(INC)/planner.h $(INC)/astar.h $
 
 .PHONY: all clean testing simulator
 
-all: $(TARGET) tests/unit_tests/tests
+all: 
+	$(TARGET) tests/unit_tests/tests 
+	$(TARGET) tests/integration_tests/tests
 
 clean:
 	rm -rf $(OBJ)
 	rm -f $(TARGET)
 
-testing: tests/unit_tests/tests
+unit_testing: tests/unit_tests/tests
+
+integration_testing: tests/integration_tests/tests
 
 $(TARGET): $(SOURCES) $(OBJ)/main.o
 	$(CXX) $(SOURCES) $(OBJ)/main.o $(LIBS) -o $(TARGET)
 
 tests/unit_tests/tests: $(SOURCES) $(INC)/planner.h $(INC)/astar.h $(INC)/taskset.h $(INC)/doctest.h
 	$(CXX) $(FLAGS) $(SOURCES) tests/unit_tests/main.cpp $(LIBS) -o tests/unit_tests/tests
+
+tests/integration_tests/tests: $(SOURCES) $(INC)/interactor.h $(INC)/doctest.h
+	$(CXX) $(FLAGS) $(SOURCES) tests/integration_tests/main.cpp $(LIBS) -o tests/integration_tests/tests
 
 # compile commands
 $(OBJ)/main.o: $(SRC)/main.cpp $(INC)/planner.h $(INC)/joint_state.h $(INC)/global_defs.h
--- tests/integration_tests/main.cpp
+++ tests/integration_tests/main.cpp
@@ -1,28 +1,27 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-
-#include <cstdio>
-
-TEST_CASE("JointState comparation")
-{
-    JointState a({1, 2});
-    JointState b({3, -4});
-    JointState c({5});
-    CHECK(a < b);
-    CHECK(c < a);
-    CHECK(c < b);
-    CHECK(a > c);
-    CHECK(a <= b);
-    CHECK(c != a);
-    CHECK(b >= c);
-    CHECK(b >= JointState({2, 2}));
-    CHECK(c == JointState({5}));
-}
-
-
-
+#include "interactor.h"
 
 TEST_CASE("Integration test")
 {
-    CHECK();
+    std::vector<double> ws = {1.0, 1.1, 1.2, 1.5, 2.0, 4.0, 10.0, 30.0, 100.0};
+    std::vector<std::string> names = {"1.0", "1.1", "1.2", "1.5", "2.0", "4.0", "10.0", "30.0", "100.0"};
+
+    for (size_t i = 0; i < ws.size(); ++i)
+    {
+        Interactor interactor("tests/integration_tests/manipulator.xml");
+        interactor.setUp({
+            3.0, // time
+            ws[i], // w
+            10000, // the number of random tests
+            TASK_POSITION, // kind of task
+            false, // random test generation
+            "scenaries/scen.log",
+            "tests/integration_tests/pyplot/7/stats_hard_w=" + names[i] + ".log",
+            "tests/integration_tests/2-dof_example.scen",
+            "tests/integration_tests/pyplot/7/runtime_hard_w=" + names[i] + ".log",
+            true // display motion
+        });
+        interactor.doMainLoop();
+    }
 }
