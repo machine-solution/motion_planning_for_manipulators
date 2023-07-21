902d7398069408ec994bdc7ab069b97718e6ad3a
ThirdAmetistEye <nn0uneim@yandex.ru>
2023-07-21 12:46:34 +0300

    attempt

--- Makefile
+++ Makefile
@@ -9,9 +9,7 @@ TARGET = simulator
 SOURCES = $(OBJ)/utils.o $(OBJ)/joint_state.o $(OBJ)/planner.o $(OBJ)/astar.o $(OBJ)/solution.o $(OBJ)/interactor.o $(OBJ)/logger.o $(OBJ)/taskset.o $(OBJ)/light_mujoco.o
 INCLUDES = $(INC)/utils.h $(INC)/joint_state.h $(INC)/planner.h $(INC)/astar.h $(INC)/solution.h $(INC)/interactor.h $(INC)/logger.h $(INC)/taskset.h $(INC)/light_mujoco.h $(INC)/global_defs.h $(INC)/doctest.h
 
-.PHONY: all clean testing simulator
-
-all: $(TARGET) tests/unit_tests/tests 
+.PHONY: all clean testing simulator 
 
 clean:
 	rm -rf $(OBJ)
@@ -27,7 +25,7 @@ $(TARGET): $(SOURCES) $(OBJ)/main.o
 tests/unit_tests/tests: $(SOURCES) $(INC)/planner.h $(INC)/astar.h $(INC)/taskset.h $(INC)/doctest.h
 	$(CXX) $(FLAGS) $(SOURCES) tests/unit_tests/main.cpp $(LIBS) -o tests/unit_tests/tests
 
-tests/integration_tests/tests: $(SOURCES) $(INC)/interactor.h $(INC)/doctest.h
+tests/integration_tests/tests: $(SOURCES) $(INC)/interactor.h $(INC)/doctest.h $(INC)/light_mujoco.h
 	$(CXX) $(FLAGS) $(SOURCES) tests/integration_tests/main.cpp $(LIBS) -o tests/integration_tests/tests
 
 # compile commands
--- tests/integration_tests/main.cpp
+++ tests/integration_tests/main.cpp
@@ -1,27 +1,22 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "interactor.h"
+#include "light_mujoco.h"
 
 TEST_CASE("Integration test")
 {
-    std::vector<double> ws = {1.0, 1.1, 1.2, 1.5, 2.0, 4.0, 10.0, 30.0, 100.0};
-    std::vector<std::string> names = {"1.0", "1.1", "1.2", "1.5", "2.0", "4.0", "10.0", "30.0", "100.0"};
-
-    for (size_t i = 0; i < ws.size(); ++i)
-    {
-        Interactor interactor("tests/integration_tests/manipulator.xml");
-        interactor.setUp({
-            3.0, // time
-            ws[i], // w
-            10000, // the number of random tests
-            TASK_POSITION, // kind of task
-            false, // random test generation
-            "scenaries/scen.log",
-            "tests/integration_tests/pyplot/7/stats_hard_w=" + names[i] + ".log",
-            "tests/integration_tests/2-dof_example.scen",
-            "tests/integration_tests/pyplot/7/runtime_hard_w=" + names[i] + ".log",
-            true // display motion
-        });
-        interactor.doMainLoop();
-    }
+    Interactor interactor("tests/integration_tests/manipulator.xml");
+    interactor.setUp({
+        3.0, // time
+        1.0, // w
+        10000, // the number of random tests
+        TASK_POSITION, // kind of task
+        false, // random test generation
+        "scenaries/scen.log",
+        "tests/integration_tests/pyplot/7/stats_hard_w=1.0.log",
+        "tests/integration_tests/2-dof_example.scen",
+        "tests/integration_tests/pyplot/7/runtime_hard_w=1.0.log",
+        true // display motion
+    });
+    interactor.doMainLoop();
 }
