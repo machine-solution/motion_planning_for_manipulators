66fcf16e932929008918d642564b342623adc958
ThirdAmetistEye <nn0uneim@yandex.ru>
2023-07-25 17:45:05 +0300

    edits

--- tests/integration_tests/main.cpp
+++ tests/integration_tests/main.cpp
@@ -4,28 +4,36 @@
 #include "interactor.h"
 
 #include <stdbool.h> 
+#include <set>
 
 #include <mujoco/mujoco.h>
 #include <GLFW/glfw3.h>
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fstream>
 
 TEST_CASE("Integration test")
 {
-    Interactor interactor("tests/integration_tests/manipulator.xml");
-    interactor.setUp({
-        3.0, // time
-        1.0, // w
-        10000, // the number of random tests
-        TASK_POSITION, // kind of task
-        false, // random test generation
-        "scenaries/scen.log",
-        "tests/integration_tests/pyplot/7/stats_hard_w=1.0.log",
-        "tests/integration_tests/2-dof_example.scen",
-        "tests/integration_tests/pyplot/7/runtime_hard_w=1.0.log",
-        false // display motion
-    });
-    interactor.doMainLoop();
+    std::vector<double> ws = {1.0, 1.1, 1.2, 1.5, 2.0, 4.0, 10.0, 30.0, 100.0};
+    std::vector<std::string> names = {"1.0", "1.1", "1.2", "1.5", "2.0", "4.0", "10.0", "30.0", "100.0"};
+
+    for (size_t i = 0; i < ws.size(); ++i)
+    {
+        Interactor interactor("tests/integration_tests/manipulator.xml");
+        interactor.setUp({
+            3.0, // time
+            ws[i], // w
+            2, // the number of random tests
+            TASK_STATE, // kind of task
+            false, // random test generation
+            "scenaries/scen.log",
+            "pyplot/7/stats_hard_w=" + names[i] + ".log",
+            "tests/integration_tests/2-dof_example.scen",
+            "pyplot/7/runtime_hard_w=" + names[i] + ".log",
+            true // display motion
+        });
+        interactor.doMainLoop();
+    }
 }
