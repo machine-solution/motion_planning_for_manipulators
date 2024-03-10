c2b668aee17bc6c47047d73f69b9e4eb8adf7480
machine-solution <machine.solution353@gmail.com>
2024-03-10 11:06:31 +0300

    patch global defs

--- include/global_defs.h
+++ include/global_defs.h
@@ -2,11 +2,13 @@
 
 #include <cmath>
 
-const int g_units = 128; // the number of planner units from [0, pi]
+const int g_worldUnits = 1024; // the number of world units from [0, pi]
+const int g_jumpSize = 8; // size of jump in world units
+
+const int g_units = 64; // the number of planner units from [0, pi]
 const double g_eps = (M_PI / g_units); // length of 1 planner unit
-const int g_unitSize = 8; // the number of world units in planner unit;
-const int g_checkJumps = 1; // the number of jumps in check collision action, must divide g_unitSize
-const int g_worldUnits = g_units * g_unitSize;
+const int g_unitSize = g_worldUnits / g_units; // the number of world units in planner unit;
+const int g_checkJumps = g_unitSize / g_jumpSize; // the number of jumps in check collision action, must divide g_unitSize
 const double g_worldEps = (M_PI / g_worldUnits);
 
 using CostType = float;
